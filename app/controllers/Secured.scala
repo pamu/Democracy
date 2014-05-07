26e4113fb844fb432fc11c9b8eb3ef0cfa361278
pamu <nagarjuna.pamu@gmail.com>
2014-05-07 17:57:32 -0400

    7th may 2014 5:57pm commit

--- app/controllers/Secured.scala
+++ app/controllers/Secured.scala
@@ -17,13 +17,6 @@ trait Secured {
    */
   private def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Auth.loginPage())
   
-   /** 
-   * Action for authenticated users.
-   */
-  def IsAuthenticated(parser: BodyParser[AnyContent])(f: => String => Request[AnyContent] => Result) = Security.Authenticated(email, onUnauthorized) { user =>
-    Action(parser)(request => f(user)(request))
-  }
-  
   /**
    * 
    */
@@ -36,17 +29,6 @@ trait Secured {
    * You will need to implement UserDAO.findOneByEmail
    */
   def withUser(f: User => Request[AnyContent] => Result) = IsAuthenticated{ email => implicit request =>
-    UserDAO.findOneByEmail(email) match {
-      case Some(user) => f(user)(request)
-      case None => Results.Forbidden//onUnauthorized(request)
-    }
-  }
-  
-  /**
-   * This method shows how you could wrap the isAuthenticated method to also fetch your user
-   * You will need to implement UserDAO.findOneByEmail
-   */
-  def withUser(parser: BodyParser[AnyContent])(f: User => Request[AnyContent] => Result) = IsAuthenticated{ email => implicit request =>
     UserDAO.findOneByEmail(email) match {
       case Some(user) => f(user)(request)
       case None => Results.Forbidden//onUnauthorized(request)
